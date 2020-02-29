9a3d25ee5dd74ca0fe9d063c33fb15ba6b1c7449
AngelidisChris <45776886+AngelidisChris@users.noreply.github.com>
2020-02-29 16:23:51 +0200

    first commit

---
+++ src/Submission/Presentation/SubmissionController.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace SocialNews\Submission\Presentation;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+final class SubmissionController
+{
+    public function show(Request $request) :Response
+    {
+        $content = 'Submission Controller';
+        return new Response($content);
+    }
+}
